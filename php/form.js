20d0df4ce5df2213381fd8e983e2c9347a7f5014
Corentin D <cocod.tm@gmail.com>
2014-09-06 18:33:15 +0200

    Focus javascript

--- php/form.js
+++ php/form.js
@@ -1,12 +1,15 @@
 function valider(frm){
 	if(frm.elements['codes'].value == "") {
 		alert("Veuillez indiquer au moins un code cours ou un lien ADE");
+		frm.elements['codes'].focus();
 		return false;
 	} else if(frm.elements['semaines'].value == "") {
 		alert("Veuillez indiquer au moins une semaine");
+		frm.elements['semaines'].focus();
 		return false;
 	} else if(frm.elements['projet'].value == "") {
 		alert("Veuillez indiquer l'ID du projet");
+		frm.elements['projet'].focus();
 		return false;
 	} else {
 		return true;
